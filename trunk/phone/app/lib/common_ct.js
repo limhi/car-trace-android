5f7a541687b4ba941ad2dc49d3a3573d58813551
limhi.cpc@gmail.com <limhi.cpc@gmail.com@720d59d7-65cd-5738-964f-9173ab7cca8b>
2013-10-05 13:08:18 +0000

    整理library, 統一加上需要的require

--- trunk/phone/app/lib/common_ct.js
+++ trunk/phone/app/lib/common_ct.js
@@ -1,7 +1,7 @@
 //var args = arguments[0] || {};
-var Alloy = require('alloy');
+var Alloy = require('alloy'), _ = require("alloy/underscore")._, Backbone = require("alloy/backbone");
 var webapi = require('common_webapi');
-webapi.enableDebug();
+//webapi.enableDebug();
 var isDebug = false;
 exports.enableDebug = function() {
 	isDebug = true;
@@ -110,7 +110,6 @@ exports.cprandomMerge = function(para) {
 	});
 };
 
-
 //data.carid(, success, fail)
 exports.cprandomMatch = function(para) {
 	//檢查必要參數
@@ -153,3 +152,186 @@ exports.cprandomMatch = function(para) {
 	});
 };
 
+//data(, success, fail)
+exports.pcpnMerge = function(para) {
+	//檢查必要參數
+	if (!para) {
+		Ti.API.error('必要參數para 未提供!');
+		return;
+	}
+	if (!para.data || !_.isObject(para.data)) {
+		Ti.API.error('必要參數para.data 未正確提供!');
+		return;
+	}
+	if (!para.data.phoneid || !_.isString(para.data.phoneid)) {
+		Ti.API.error('必要參數para.data.phoneid 未正確提供!');
+		return;
+	}
+	if (!para.data.type || !_.isString(para.data.type)) {
+		Ti.API.error('必要參數para.data.type 未正確提供!');
+		return;
+	}
+	if (!para.data.message || !_.isString(para.data.message)) {
+		Ti.API.error('必要參數para.data.message 未正確提供!');
+		return;
+	}
+
+	//取得參數
+	var phoneid = para.data.phoneid;
+	var type = para.data.type;
+	var message = para.data.message;
+
+	//設定呼叫參數
+	var URL = "ctpush/v1/postPCPushNotification";
+	URL = String.format("%s/%s", URL, phoneid);
+
+	var sendObj = {
+		type : type,
+		message : message
+	};
+	//呼叫web api warpper
+	webapi.connect({
+		data : {
+			ip : Alloy.Globals.appEngineIP,
+			url : URL,
+			method : 'post',
+			json : sendObj
+		},
+		success : para.success,
+		fail : para.fail,
+	});
+};
+
+//data(, success, fail)
+exports.cppnMerge = function(para) {
+	//檢查必要參數
+	if (!para) {
+		Ti.API.error('必要參數para 未提供!');
+		return;
+	}
+	if (!para.data || !_.isObject(para.data)) {
+		Ti.API.error('必要參數para.data 未正確提供!');
+		return;
+	}
+	if (!para.data.carid || !_.isString(para.data.carid)) {
+		Ti.API.error('必要參數para.data.carid 未正確提供!');
+		return;
+	}
+	if (!para.data.type || !_.isString(para.data.type)) {
+		Ti.API.error('必要參數para.data.type 未正確提供!');
+		return;
+	}
+	if (!para.data.message || !_.isString(para.data.message)) {
+		Ti.API.error('必要參數para.data.message 未正確提供!');
+		return;
+	}
+
+	//取得參數
+	var carid = para.data.carid;
+	var type = para.data.type;
+	var message = para.data.message;
+
+	//設定呼叫參數
+	var URL = "ctpush/v1/postCPPushNotification";
+	URL = String.format("%s/%s", URL, carid);
+
+	var sendObj = {
+		type : type,
+		message : message
+	};
+	//呼叫web api warpper
+	webapi.connect({
+		data : {
+			ip : Alloy.Globals.appEngineIP,
+			url : URL,
+			method : 'post',
+			json : sendObj
+		},
+		success : para.success,
+		fail : para.fail,
+	});
+};
+
+//data(, success, fail)
+exports.pcpnSend = function(para) {
+	//檢查必要參數
+	if (!para) {
+		Ti.API.error('必要參數para 未提供!');
+		return;
+	}
+	if (!para.data || !_.isObject(para.data)) {
+		Ti.API.error('必要參數para.data 未正確提供!');
+		return;
+	}
+	if (!para.data.phoneid || !_.isString(para.data.phoneid)) {
+		Ti.API.error('必要參數para.data.phoneid 未正確提供!');
+		return;
+	}
+	if (!para.data.messageid || !_.isString(para.data.messageid)) {
+		Ti.API.error('必要參數para.data.messageid 未正確提供!');
+		return;
+	}
+
+	//取得參數
+	var phoneid = para.data.phoneid;
+	var messageid = para.data.messageid;
+
+	//設定呼叫參數
+	var URL = "ctpush/v1/postPCPushNotificationSend";
+	URL = String.format("%s/%s/%s", URL, phoneid, messageid);
+
+	var sendObj = {};
+	//呼叫web api warpper
+	webapi.connect({
+		data : {
+			ip : Alloy.Globals.appEngineIP,
+			url : URL,
+			method : 'post',
+			json : sendObj
+		},
+		success : para.success,
+		fail : para.fail,
+	});
+};
+
+//data(, success, fail)
+exports.cppnSend = function(para) {
+	//檢查必要參數
+	if (!para) {
+		Ti.API.error('必要參數para 未提供!');
+		return;
+	}
+	if (!para.data || !_.isObject(para.data)) {
+		Ti.API.error('必要參數para.data 未正確提供!');
+		return;
+	}
+	if (!para.data.carid || !_.isString(para.data.carid)) {
+		Ti.API.error('必要參數para.data.carid 未正確提供!');
+		return;
+	}
+	if (!para.data.messageid || !_.isString(para.data.messageid)) {
+		Ti.API.error('必要參數para.data.messageid 未正確提供!');
+		return;
+	}
+
+	//取得參數
+	var carid = para.data.carid;
+	var messageid = para.data.messageid;
+
+	//設定呼叫參數
+	var URL = "ctpush/v1/postCPPushNotificationSend";
+	URL = String.format("%s/%s/%s", URL, carid, messageid);
+
+	var sendObj = {};
+	//呼叫web api warpper
+	webapi.connect({
+		data : {
+			ip : Alloy.Globals.appEngineIP,
+			url : URL,
+			method : 'post',
+			json : sendObj
+		},
+		success : para.success,
+		fail : para.fail,
+	});
+};
